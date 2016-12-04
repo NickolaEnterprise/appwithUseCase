a96b40f1cbd28c5a0ddafc49a6244fd9403ba3a4
HudkovNickola <hudkovnickola@gmail.com>
2016-12-04 18:48:42 +0300

    Gulp failures with timeout

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Николай on 04.12.2016.
+ */
+var protractor = require('gulp-protractor').protractor;
+var gulp = require('gulp');
+var webdriver_standalone = require("gulp-protractor").webdriver_standalone;
+var connect = require('gulp-connect');
+
+gulp.task('StartServer', function() {
+    connect.server(
+        {
+            port: 1818
+        }
+    );
+});
+
+gulp.task('StartWD', webdriver_standalone);
+
+gulp.task('StartPT', function() {
+    gulp.src(['test/hudkov.js']).pipe(protractor({
+        configFile: 'test/protractor.c.js',
+        args: ['--baseUrl', 'http://localhost:1818']
+    })).on('error', function(e) {
+        console.log(e)
+    });
+});
+gulp.task('default',['StartServer','StartWD','StartPT']);
+gulp.task('StartSWD',['StartServer','StartWD']);
