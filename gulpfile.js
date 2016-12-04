4c61f2e53224cfc1d6491c86957936b329a6084f
HudkovNickola <HudkovNickola@users.noreply.github.com>
2016-12-04 22:15:06 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 /**
  * Created by Николай on 04.12.2016.
  */
-var protractor = require('gulp-protractor').protractor;
+var gulpProtractorAngular = require('gulp-angular-protractor');
 var gulp = require('gulp');
-var webdriver_update = require("gulp-protractor").webdriver_update;
 var webdriver_standalone = require("gulp-protractor").webdriver_standalone;
 var connect = require('gulp-connect');
 
@@ -14,16 +13,16 @@ gulp.task('StartServer', function() {
         }
     );
 });
-gulp.task('StartWU', webdriver_update);
+
 gulp.task('StartWD', webdriver_standalone);
 
-gulp.task('StartPT', function() {
-    gulp.src(['test/hudkov.js']).pipe(protractor({
-        configFile: 'test/protractor.c.js'
-    })).on('error', function(e) {
-        console.log(e)
-    });
+gulp.task('StartProtractor', function(callback) {
+    gulp.src(['test/hudkov.js'])
+        .pipe(gulpProtractorAngular({
+            configFile: 'test/protractor.c.js'
+        })).on('error', function(e) {
+            console.log(e);
+        }).on('end', callback);
 });
-gulp.task('default',['StartServer','StartWD','StartPT']);
-gulp.task('StartSWD',['StartServer','StartWD']);
-gulp.task('StartSWU',['StartWU']);
+
+gulp.task('default',['StartServer','StartWD','StartProtractor']);
